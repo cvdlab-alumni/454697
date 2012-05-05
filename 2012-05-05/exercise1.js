7d3efb6515483119287aa5bd674d028ab8111137
Elisa Lamberti <wlapiaja@gmail.com>
2012-05-05 17:24:09 +0300

    Update 2012-05-05/exercise1.js

--- 2012-05-05/exercise1.js
+++ 2012-05-05/exercise1.js
@@ -1,10 +1,11 @@
+
 //SOPWITH PUP
 
 //ALA BASSA DX
 
 var domain1 = INTERVALS(1)(30);
 var domain2 = DOMAIN([[0, 1], [0, 1]])([15, 30]);
-//ar c1 = BEZIER(S0)(controls);
+//var c1 = BEZIER(S0)(controls);
 //var curve1 = MAP(c1)(domain1);
 //DRAW(curve1);
 
@@ -50,7 +51,7 @@ var wingLSX = MAP(surf1)(domain2);
 
 //ALA SUPERIORE INTERA
 
-var wingUDX=T([2])([1])(wingLDX);
+var wingUDX=T([2])([1.5])(wingLDX);
 var p5 = p0.map(function(p){return[p[0], p[1]-1, p[2]]});
 var p6 = p0.map(function(p){return[p[0], p[1]-2, p[2]]});
 var p7 = p0.map(function(p){return[p[0], p[1]-3, p[2]]});
@@ -67,7 +68,7 @@ var c9 = BEZIER(S0)(p9);
 
 
 var surf1 = BEZIER(S1)([c0, c5,c6, c7, c8, c9, c10]);
-var wingUSX =T([2])([1])(MAP(surf1)(domain2));
+var wingUSX =T([2])([1.5])(MAP(surf1)(domain2));
 var wings = STRUCT([wingUSX, wingLDX, wingUDX, wingLSX]);
 //STRUTTURA ALI
 DRAW(COLOR([0.8, 0.4, 0.2])(wings));
